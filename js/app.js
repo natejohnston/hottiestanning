7590dbe08d7d9a0605beb3cca75715fb5d70a4d4
natejohnston <njohnston@gmail.com>
2022-03-24 22:32:17 -0600

    finalizing sites

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 function onSubmit(token) {
     document.getElementById("contactForm").submit();
 }
+
+function scrollToServices() {
+    document.getElementById("services").scrollIntoView();
+}
+
+function scrollToContactForm() {
+    document.getElementById("contactForm").scrollIntoView();
+}
